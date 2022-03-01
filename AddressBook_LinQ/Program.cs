b037ff44ecafe66efa26ad877d844f3c7c61ca6e
Ingole987 <pratik.ingole98@gmail.com>
2022-03-01 20:30:12 +0530

    Feat: Ability To Insert New Contacts

--- AddressBook_LinQ/Program.cs
+++ AddressBook_LinQ/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AddressBook_LinQ;
+using System.Data;
 
 Console.WriteLine("Welcome To Address Book Using LINQ !");
 Console.WriteLine("------------------------------------");
 AddressBookTable addressBookTable = new AddressBookTable();
+
+DataTable data = addressBookTable.CreateAddressBookDataTable();
+addressBookTable.DisplayContacts(data);
