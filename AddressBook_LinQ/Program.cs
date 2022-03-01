93e68a90bc3ca112b83eaa37fc991cd082dd00e1
Ingole987 <pratik.ingole98@gmail.com>
2022-03-01 20:24:18 +0530

    Feat: Ability To Make Tables And Name Coloumn

--- AddressBook_LinQ/Program.cs
+++ AddressBook_LinQ/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook_LinQ;
+
 Console.WriteLine("Welcome To Address Book Using LINQ !");
 Console.WriteLine("------------------------------------");
 AddressBookTable addressBookTable = new AddressBookTable();
