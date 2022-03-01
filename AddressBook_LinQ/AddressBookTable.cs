0312b7c776940e93f08a3cf100a4bf707ae03e2c
Ingole987 <pratik.ingole98@gmail.com>
2022-03-01 20:20:34 +0530

    Feat: Ability To Create DataBase

---
+++ AddressBook_LinQ/AddressBookTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LinQ
+{
+    public class AddressBookTable
+    {
+        public void CreateAddressBookDataTable()
+        {
+            //DataTable 
+            DataTable dataTable = new DataTable();
+        }
+    }
+}
---
+++ AddressBook_LinQ/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome To Address Book Using LINQ !");
+Console.WriteLine("------------------------------------");
+AddressBookTable addressBookTable = new AddressBookTable();
